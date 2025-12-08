2674b4286087593fc8ef606fd19320bebca99c94
ChauNgocMinh <chaungocminh202@gmail.com>
2025-12-08 21:32:44 +0700

    update

--- BookingRoomHotel/Models/ModelsInterface/IUploadFileService.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace BookingRoomHotel.Models
-{
-    public interface IUploadFileService
-    {
-        public string uploadImage(IFormFile url, string path);
-        List<Media> uploadListImage(IFormFileCollection images, string path);
-    }
-}
